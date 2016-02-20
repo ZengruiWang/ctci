faf7154a1415fa133d6222f9a85c381a13f0ced3
Zengrui Wang <zengruiwang@gmail.com>
2016-02-19 20:47:18 -0800

    Update Question4_9.cpp
    Another implementation of 4.9

--- c++/Chapter 4/Question4_9.cpp
+++ c++/Chapter 4/Question4_9.cpp
@@ -1,65 +1,42 @@
-#include<iostream>
-using namespace std;
-
-struct node{
-    int data;
-    node *right, *left;
+#include <vector>
+#include <iostream>
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-
-void createMinimalBst(node* &root, int arr[], int start, int end){
-    if(start>end)
-        return;
-    if(root==NULL){
-        node *ptr = new node;
-        int ind = start + (end-start)/2;
-        ptr->data = arr[ind];
-        ptr->left = NULL;
-        ptr->right = NULL;
-        root = ptr;
-        createMinimalBst(root->left, arr, start, ind-1);
-        createMinimalBst(root->right, arr, ind+1, end);
-    }
+void dfs(TreeNode* root, const int &target, vector<int> &path, vector<vector<int>> &res){
+	if(!root) return;
+	path.push_back(root->val);
+	int sum = 0;
+	for(int i = path.size() - 1; i >= 0; --i){
+		sum += path[i];
+		if(sum == target){
+			res.push_back(vector<int>(path.begin() + i, path.end()));
+		}
+	}
+	dfs(root->left, target, path, res);
+	dfs(root->right, target, path, res);
+	path.pop_back();
 }
-
-void printIfSum(int* arr, int sum, int len){
-    for(int i=0; i<len; i++){
-        int s=0;
-        for(int j=i; j<len; j++){
-            s += arr[j];
-            if(s == sum){
-                for(int k=i; k<=j; k++)
-                    cout<<arr[k]<<" ";
-                cout<<'\n';
-            }
-        }
-    }
-}
-
-void print(node *root, int sum, int *path, int level){
-    if(root==NULL){
-        return;
-    }
-    path[level] = root->data;
-    int s = 0;
-    for(int i=level; i>=0; i--){
-        s += path[i];
-        if(s==sum){
-            for(int j=level; j>=i; j--)
-                cout<<path[j]<<" ";
-            cout<<'\n';
-        }
-    }
-    if(root->left)
-        print(root->left, sum, path, level+1);
-    if(root->right)
-        print(root->right, sum, path, level+1);
-}
-
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    node *root;
-    root = NULL;
-    createMinimalBst(root, arr, 0, 8);
-    int path[100];
-    print(root, 7, path, 0);
+	TreeNode *root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->left->left = new TreeNode(3);
+	root->left->right = new TreeNode(1);
+	root->left->right->left = new TreeNode(2);
+	root->right = new TreeNode(1);
+	root->right->left = new TreeNode(2);
+	root->right->left->left = new TreeNode(4);
+	vector<int> path;
+	vector<vector<int>> res;
+	dfs(root, 3, path, res);
+	for(int i = 0; i < res.size(); ++i){
+		for(int j = 0; j < res[i].size(); ++j){
+			cout<< res[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
 }
