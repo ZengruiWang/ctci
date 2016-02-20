503cb7a93e985f29c886219e45a69cdeb6f65dfe
Zengrui Wang <zengruiwang@gmail.com>
2016-02-20 09:59:55 -0800

    Update Question5_3.cpp

--- c++/Chapter 5/Question5_3.cpp
+++ c++/Chapter 5/Question5_3.cpp
@@ -21,59 +21,55 @@ void printBinary(int n){
     }
     cout<<'\n';
 }
-
-void next_smallest(int n){
-	int len = 0;
-	int tmp = n;
-	int res;
-	while(len<31){
-		if((tmp&1) == 0){
-			tmp >>= 1;
-			len++;
-			if(tmp&1 == 1){
-				//10 occured
-				res = n - (1<<len) + (1<<(len-1)); 
-				printBinary(res);
-				return;
-			}
-		}
-		else{
-			tmp >>=1;
-			len++;
+int getNextLargest(int n){
+	int c1 = 0, i;
+	//find the right-most non-trailing 0
+	for(i = 0; i < 32; ++i){
+		if((n & (1 << i)) == 0 && c1 > 0){
+			//set the ith bit to 1
+			n |= (1 << i);
+			break;
+		}else if((n & (1 << i)) != 0){
+			c1++;
 		}
 	}
-	//All 1's
-	cout<<INT_MIN<<'\n';
+	if(i == 32) return -1;
+	//clear lower [0, i - 1] bit
+	int allOnes = ~0;
+	n &= (allOnes << i);
+	//get the mask which has c1 - 1 1's in the lower [0, c1 - 2]
+	int mask = (1 << (c1 - 1)) - 1;
+	return mask | n;
 }
-
-void next_largest(int n){
-	int len = 0;
-	int tmp = n;
-	int res;
-	while(len<31){
-		if(tmp&1 == 1){
-			tmp >>= 1;
-			len++;
-			if((tmp&1) == 0){
-				//01 occured
-				res = n + (1<<len) - (1<<(len-1));
-				printBinary(res);
-				return;
-			}
-		}
-		else{
-			tmp >>=1;
-			len++;
+int getNextSmallest(int n){
+	int c0 = 0, i;
+	for(i = 0; i < 32; ++i){
+		if((n & (1 << i)) != 0 && c0 > 0){
+			//clear the ith bit 
+			n &= ~(1 << i);
+			printBinary(n);
+			break;
+		}else if((n & (1 << i))  == 0){
+			c0++;
 		}
 	}
-	cout<<INT_MAX;
+	if(i == 32) return -1;
+	//set lower [0, i - 1] to 1's
+	n |= (1 << i) - 1;
+	printBinary(n);
+	//clear lower c0 - 1 bits
+	n &= (~0)<<(c0 - 1);
+	printBinary(n);
+	return n;
 }
-
 int main(){
-	int a = (1<<31)+(1<<29);
-	cout<<a<<'\n';
-	printBinary(a);
-	next_smallest(a);
-	next_largest(a);
-    return 0;
+	int n = 13948;
+	printBinary(n);
+	int res = getNextSmallest(n);
+	cout<<"result:"<<endl;
+	printBinary(res);
+	cout<<res<<endl;
 }
+}
+
+
